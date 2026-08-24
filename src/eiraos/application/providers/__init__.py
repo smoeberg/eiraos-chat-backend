91ca3f1bc46ce02aaf430d3029b8f949010ceda7
WM-group development - Eira <moeberg@wm-group.dk>
2026-08-24 09:59:27 +0200

    chore: export governed provider policy components

---
+++ src/eiraos/application/providers/__init__.py
@@ -0,0 +1,4 @@
+from eiraos.application.providers.governed import GovernedAIProvider
+from eiraos.application.providers.policy import authorize_provider_model
+
+__all__ = ["GovernedAIProvider", "authorize_provider_model"]
